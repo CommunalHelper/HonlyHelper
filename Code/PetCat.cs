0521d0456d11a3010cb0b6b9c7174648d3190e0d
AAA1459 <72622056+AAA1459@users.noreply.github.com>
2022-12-01 22:35:45 +0800

    Adjust "Player pet Cat"s related Sprite Code
    Let the "Player pet Cat" texture be set in Sprites.xml
    Let SkinModHelper's custom Xml can get that texture

--- Code/PetCat.cs
+++ Code/PetCat.cs
@@ -6,33 +6,34 @@ using System.Collections;
 namespace Celeste.Mod.HonlyHelper {
     [CustomEntity("HonlyHelper/PettableCat")]
     public class PettableCat : NPC {
+
         private readonly Sprite CatSprite;
         private readonly Sprite ThePetterSprite;
+
         private readonly string catFlag;
         private Coroutine pettingRoutine;
         private Vector2 CatAnchor;
         private bool PettingInProgress = false;
         private Vector2 friendposition;
 
-        public PettableCat(Vector2 position, string CatFlag)
-           : base(position) {
-            CatSprite = new Sprite(GFX.Game, "characters/HonlyHelper/pettableCat/");
+        public PettableCat(Vector2 position, string CatFlag, string texture = "objects/HonlyHelper/pettableCat/")
+             : base(position)
+        {
+            ThePetterSprite = GFX.SpriteBank.Create("HonlyHelper_CatPetter");
+            Add(ThePetterSprite);
+
+            CatSprite = new Sprite(GFX.Game, texture);
             CatSprite.AddLoop("idle", "spoons_idle", 0.15f);
             CatSprite.Add("pet", "spoons_pet", 0.15f);
             Add(CatSprite);
-
-            ThePetterSprite = new Sprite(GFX.Game, "characters/HonlyHelper/pettableCat/ThePetter/");
-            ThePetterSprite.AddLoop("idle", "player_idle", 1f);
-            ThePetterSprite.AddLoop("pet", "player_pet", 0.15f);
-            Add(ThePetterSprite);
-            ThePetterSprite.Position = CatSprite.Position + new Vector2(-22f, -24f);
 
             catFlag = CatFlag ?? "CatHasBeenPet";
         }
 
         public PettableCat(EntityData data, Vector2 offset)
-            : this(data.Position + offset, data.Attr("catFlag")) {
-        }
+             : this(data.Position + offset, data.Attr("catFlag"))
+        { }
+
 
         public override void Added(Scene scene) {
             base.Added(scene);
@@ -40,24 +41,30 @@ namespace Celeste.Mod.HonlyHelper {
             CatAnchor = CatSprite.Position;
         }
 
-        public override void Awake(Scene scene) {
+        public override void Awake(Scene scene)
+        {
             base.Awake(scene);
             CatSprite.Play("idle");
             ThePetterSprite.Play("idle");
         }
 
-        private void OnPetting(Player player) {
+        private void OnPetting(Player player)
+        {
             Level.StartCutscene(OnPettingEnd);
             Add(pettingRoutine = new Coroutine(ThePetting(player)));
         }
 
-        private IEnumerator ThePetting(Player player) {
-            yield return PlayerApproachLeftSide(player, turnToFace: true, 6f); 
+        private IEnumerator ThePetting(Player player)
+        {
+            yield return PlayerApproachLeftSide(player, turnToFace: true, 6f);
             friendposition = player.Sprite.Position;
             PettingInProgress = true;
             CatSprite.Play("pet");
             CatSprite.Position = CatAnchor + new Vector2(-4f, -8f);
+
             ThePetterSprite.Play("pet");
+            ThePetterSprite.Position = friendposition + new Vector2(-22f, -24f);
+
             player.Sprite.Position = friendposition + (Vector2.UnitY * 1000f);
             Audio.Play("event:/HonlyHelper/catsfx", Center);
             yield return 2f;
@@ -68,21 +75,22 @@ namespace Celeste.Mod.HonlyHelper {
             SceneAs<Level>().Session.SetFlag(catFlag);
         }
 
-        private void OnPettingEnd(Level level) {
+        private void OnPettingEnd(Level level)
+        {
             Player entity = Scene.Tracker.GetEntity<Player>();
-            if (entity != null) {
+            if (entity != null)
+            {
                 entity.StateMachine.Locked = false;
                 entity.StateMachine.State = Player.StNormal;
             }
-
             pettingRoutine.Cancel();
             pettingRoutine.RemoveSelf();
             CatSprite.Play("idle");
             CatSprite.Position = CatAnchor;
-            if (PettingInProgress == true) {
+            if (PettingInProgress == true)
+            {
                 entity.Sprite.Position = friendposition;
             }
-
             entity.Sprite.Play("idle");
             ThePetterSprite.Play("idle");
             PettingInProgress = false;
